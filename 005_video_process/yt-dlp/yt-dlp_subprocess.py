fad0e9373f34ef721c6d6768214f34a0adfeeab6
shade <382961818@qq.com>
2023-10-17 10:58:35 +0800

    modified:   005_video_process/yt-dlp/conf/www.bilibili.com_cookies.txt 	renamed:    005_video_process/yt-dlp/list/bak/fang_talk.downlist.bak -> 005_video_process/yt-dlp/downlist/bak/fang_talk.downlist 	renamed:    005_video_process/yt-dlp/list/white_board_ASML.downlist -> 005_video_process/yt-dlp/downlist/bak/white_board_ASML.downlist 	renamed:    005_video_process/yt-dlp/list/bak/zhai_talk.downlist.bak -> 005_video_process/yt-dlp/downlist/bak/zhai_talk.downlist 	new file:   005_video_process/yt-dlp/downlist/tan3.downlist 	deleted:    005_video_process/yt-dlp/list/tan3.downlist 	modified:   005_video_process/yt-dlp/yt-dlp_subprocess.py

--- 005_video_process/yt-dlp/yt-dlp_subprocess.py
+++ 005_video_process/yt-dlp/yt-dlp_subprocess.py
@@ -5,12 +5,12 @@ import os
 
 # Configurable paths
 downlist_dir = os.path.join(os.environ['USERPROFILE'], 'OneDrive',
-                            '00_source', 'testCode', '005_video_process', 'yt-dlp', 'list')
+                            '00_source', 'testCode', '005_video_process', 'yt-dlp', 'downlist')
 CONFIG_DIR = os.path.join(os.environ['USERPROFILE'], 'OneDrive',
                           '00_source', 'testCode', '005_video_process', 'yt-dlp', 'conf')
 
 
-def download_video(playlist, line, config_file, download_folder):
+def download_video_yt_dlp(playlist, line, config_file, download_folder):
     config_location = os.path.join(CONFIG_DIR, config_file)
     cmd = [
         'yt-dlp',
@@ -24,6 +24,23 @@ def download_video(playlist, line, config_file, download_folder):
         subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Subprocess failed with error: {e}")
+
+
+def download_video_you_get(playlist, url, config_file, download_folder):
+
+    command = ["you-get",
+
+               '-o', os.path.expanduser(
+                   f'~/{download_folder}/{playlist}/'),
+               "-l",
+               url,
+               "--debug"]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to download video: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def download_video_mul(playlist="list1"):
@@ -42,64 +59,10 @@ def download_video_mul(playlist="list1"):
     for i, line in enumerate(lines, start=1):
         print(f"Start to download {i}: {line.strip()}")
         if ".bilibili." in line:
-            download_video(playlist, line, 'yt-dlp_bili.conf', 'bili')
+            download_video_you_get(playlist, line, 'yt-dlp_bili.conf', 'bili')
         elif ".youtube." in line:
-            download_video(playlist, line, 'yt-dlp_YouTube.conf', 'YouTube')
-
-
-def download_vid_mul(playlist="list1"):
-    # Define the file path in a more portable way
-    downlist_dir = os.path.join(os.environ['USERPROFILE'], 'OneDrive',
-                                '00_source', 'testCode', '005_video_process', 'yt-dlp', 'list')
-    for file in os.listdir(downlist_dir):
-        if file.endswith(".downlist"):
-            print(f"Downloading from {file}")
-            playlist = file.split(".")[0]
-
-            file_path = os.path.join(downlist_dir, file)
-            try:
-                with open(file_path, 'r', encoding='utf-8') as f1:
-                    lines = f1.readlines()
-            except FileNotFoundError:
-                print(f"File not found: {file_path}")
-                return
-
-            for i, line in enumerate(lines, start=1):
-                line = line.strip()
-                print(f"start to download {i}: {line}")
-
-                if ".bilibili." in line:
-                    config_location_bili = os.path.join(
-                        '~', 'OneDrive', '00_source', 'testCode', '005_video_process', 'yt-dlp', 'conf', 'yt-dlp_bili.conf')
-                    cmd = [
-                        'yt-dlp',
-                        '--config-locations', config_location_bili,
-                        # Save all videos under YouTube directory in your home directory
-                        '-o', os.path.expanduser(
-                            f'~/bili/{playlist}/%(autonumber)s_%(title)s-[%(id)s]-.%(ext)s'),
-                        line.strip()  # Stripping the newline character from the URL
-                    ]
-                    try:
-                        subprocess.run(
-                            cmd, check=True)
-                    except subprocess.CalledProcessError as e:
-                        print(f"Subprocess failed with error: {e}")
-                elif ".youtube." in line:
-                    config_location_YouTube = os.path.join(
-                        '~', 'OneDrive', '00_source', 'testCode', '005_video_process', 'yt-dlp', 'conf', 'yt-dlp_YouTube.conf')
-                    cmd = [
-                        'yt-dlp',
-                        '--config-locations', config_location_YouTube,
-                        # Save all videos under YouTube directory in your home directory
-                        '-o', os.path.expanduser(
-                            f'~/YouTube//{playlist}/%(autonumber)s_%(title)s-[%(id)s]-.%(ext)s'),
-                        line.strip()  # Stripping the newline character from the URL
-                    ]
-                    try:
-                        subprocess.run(
-                            cmd, check=True)
-                    except subprocess.CalledProcessError as e:
-                        print(f"Subprocess failed with error: {e}")
+            download_video_yt_dlp(
+                playlist, line, 'yt-dlp_YouTube.conf', 'YouTube')
 
 
 def get_playlist_details(url):
